d7511fc0e2c0834a8b8ffd0b1003adba39e03f4a
ganlvtech <ganlvtech@qq.com>
2017-06-22 21:55:29 +0800

    新建数据库迁移和数据库模型

---
+++ database/migrations/2017_06_22_132144_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type', 20)->comment('分类：course/teacher')->index();
+            $table->text('title')->comment('名称');
+            $table->text('excerpt')->comment('简介');
+            $table->longText('content')->comment('内容');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
--- database/migrations/2017_06_22_132157_create_post_metas_table.php
+++ database/migrations/2017_06_22_132157_create_post_metas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostMetasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_metas', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('post_id')->comment('post表的外键')->index();
+            $table->string('meta_key', 191)->index()->comment('键');
+            $table->text('meta_value')->comment('值');
             $table->timestamps();
         });
     }
@@ -30,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_metas');
     }
 }
