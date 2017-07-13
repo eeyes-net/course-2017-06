0123ca5b945d18daaafa5c715801b581d35049be
ganlvtech <ganlvtech@qq.com>
2017-07-13 21:48:04 +0800

    说明文档移动端适配

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link rel="stylesheet" type="text/css" href="/css/markdown.css">
 </head>
 <body>
